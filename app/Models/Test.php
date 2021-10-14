c42dab86404555d7991c7f0a12858472e0daccf7
maikel <kutaj74@gmail.com>
2021-10-14 22:18:13 +0200

    defined orm relations

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Test extends Model
 {
     use HasFactory;
+
+    public function testCategories(): BelongsToMany
+    {
+        return $this->belongsToMany(TestCategory::class);
+    }
+
 }
--- app/Models/TestCategory.php
+++ app/Models/TestCategory.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class TestCategory extends Model
 {
     use HasFactory;
+
+    public function tests(): BelongsToMany
+    {
+        return $this->belongsToMany(Test::class);
+    }
+
 }
