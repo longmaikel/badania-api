cfab3d0bc4ebf359fb950e2d910e3642519b7c00
maikel <kutaj74@gmail.com>
2021-10-15 19:34:16 +0200

    created routes

--- routes/api.php
+++ routes/api.php
@@ -15,19 +15,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::prefix('api')->group(function() {
 
-   Route::get('tests', [TestController::class, 'index']);
-   Route::post('tests', [TestController::class, 'store']);
-   Route::patch('tests/{test}', [TestController::class, 'update']);
-   Route::delete('tests/{test}', [TestController::class, 'destroy']);
+Route::get('tests', [TestController::class, 'index']);
+Route::post('tests', [TestController::class, 'store']);
+Route::patch('tests/{test}', [TestController::class, 'update']);
+Route::delete('tests/{test}', [TestController::class, 'destroy']);
 
-    Route::get('test-categories', [TestCategoryController::class, 'index']);
-    Route::post('test-categories', [TestCategoryController::class, 'store']);
-    Route::patch('test-categories/{test_category}', [TestCategoryController::class, 'update']);
-    Route::delete('test-categories/{test_category}', [TestCategoryController::class, 'destroy']);
+Route::get('test-categories', [TestCategoryController::class, 'index']);
+Route::post('test-categories', [TestCategoryController::class, 'store']);
+Route::patch('test-categories/{test_category}', [TestCategoryController::class, 'update']);
+Route::delete('test-categories/{test_category}', [TestCategoryController::class, 'destroy']);
 
-});
 //
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
