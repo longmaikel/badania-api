402a7ac46836da44c12a4ff6be2d4828f8b85d42
maikel <kutaj74@gmail.com>
2021-10-15 18:21:53 +0200

    created routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\TestCategoryController;
+use App\Http\Controllers\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('api')->group(function() {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+   Route::get('tests', [TestController::class, 'index']);
+   Route::post('tests', [TestController::class, 'store']);
+   Route::patch('tests/{test}', [TestController::class, 'update']);
+   Route::delete('tests/{test}', [TestController::class, 'destroy']);
+
+    Route::get('test-categories', [TestCategoryController::class, 'index']);
+    Route::post('test-categories', [TestCategoryController::class, 'store']);
+    Route::patch('test-categories/{test_category}', [TestCategoryController::class, 'update']);
+    Route::delete('test-categories/{test_category}', [TestCategoryController::class, 'destroy']);
+
 });
+//
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
