809cc29d33bda9b40d5bd9fea9e9488a363ee6e1
maikel <kutaj74@gmail.com>
2021-10-15 19:39:35 +0200

    create get tests endpoint

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TestsResource;
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function index(): TestsResource
+    {
+        $tests = Test::all();
+        return new TestsResource($tests);
+    }
 }
