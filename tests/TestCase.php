1e6205d5aeec14101d5b98fe8431a04a5361bbf0
maikel <kutaj74@gmail.com>
2021-10-20 17:05:27 +0200

    changed base test case - add faker

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Faker\Factory;
+use Faker\Generator as Faker;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected Faker $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+    }
 }
