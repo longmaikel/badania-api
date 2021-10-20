4373c3128faa466820df5199ef606ae8cdeece82
maikel <kutaj74@gmail.com>
2021-10-20 17:59:57 +0200

    created test checking incomplete create test data cases

--- tests/Feature/TestControllerTest.php
+++ tests/Feature/TestControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Test;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\TestCase;
@@ -9,20 +10,60 @@ use Tests\TestCase;
 class TestControllerTest extends TestCase
 {
 
+    /**
+     * @var string[]
+     */
+    private array $singleTestModelFieldsStructure;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->singleTestModelFieldsStructure = [
+            "id",
+            "name",
+            "price",
+            "created_at",
+            "updated_at"
+        ];
+    }
+
     public function test_index_return_valid_json(): void
     {
         $this->json('get', 'api/tests')
             ->assertStatus(Response::HTTP_OK)
             ->assertJsonStructure([
                 'data' => [
-                    '*' => [
-                        "id",
-                        "name",
-                        "price",
-                        "created_at",
-                        "updated_at"
-                    ]
+                    '*' => $this->singleTestModelFieldsStructure
                 ]
             ]);
+    }
+
+    public function test_creating_test(): void
+    {
+        $data = ['name' => 'example test', 'price' => '230.99'];
+        $test = Test::create($data);
+
+        $this->json('post', 'api/tests', $data)
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure(['test' => $this->singleTestModelFieldsStructure]);
+        $this->assertDatabaseHas('tests',$data);
+
+    }
+
+    /**
+     * @dataProvider incompleteParamsCreateTestRequestDataProvider
+     */
+    public function test_creating_test_with_no_each_required_params(array $data): void
+    {
+        $this->json('post', 'api/tests', $data)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    public function incompleteParamsCreateTestRequestDataProvider(): array
+    {
+        return [
+            [['name' => 'example test']],
+            [['price' => '230.99']]
+        ];
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,12 @@ namespace Tests;
 
 use Faker\Factory;
 use Faker\Generator as Faker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseTransactions;
 
     protected Faker $faker;
 
